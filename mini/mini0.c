a6c4fb302df7bfeb852d6b44132a59e922d11d25
Quoc Tri Do <qdo@1-B-25.42heilbronn.de>
2024-05-13 01:13:40 +0200

    first finishe check syntax

--- mini/mini0.c
+++ mini/mini0.c
@@ -6,7 +6,7 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 04:07:23 by qdo               #+#    #+#             */
-/*   Updated: 2024/05/12 23:45:56 by qdo              ###   ########.fr       */
+/*   Updated: 2024/05/13 01:11:33 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	main(int ac, char **av, char **env)
 	// ft_strcpy(str,
 	// 	 "1(2(3)4(5(6)7(8)9 1)2s34f5 )6|7 8|9 ||1c0d2|3|4a5b6 7s8f9f1 0%2%34 56\" 7'8\"");
 	// ft_strcpy(str, "1 |(23)46|7 8|9 ||1c0d2|");
-	ft_strcpy(str, "1 || (23)");
+	ft_strcpy(str, "<$> | $.");
 	if (ac != 0)
 		av = 0;
 	if (ft_pre_check_syntax(str) == 0)
@@ -35,7 +35,7 @@ int	main(int ac, char **av, char **env)
 	mini0(&mini, str, env);
 	if (sub_mini0(&mini) == 0)
 	{
-		printf("call free_mini\n");
+		// printf("call free_mini\n");
 		free_mini(&mini);
 	}
 	mini0(&mini, 0, 0);
