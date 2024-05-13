ac8f8a8b22e078c11601b9b767d32a0a3ecaf1e8
bpisak-l <bpisak-l@student.42.fr>
2024-05-13 12:05:31 +0200

    Add basic version of cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: bpisak-l <bpisak-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 11:05:00 by bpisak-l          #+#    #+#             */
-/*   Updated: 2024/05/13 11:53:33 by bpisak-l         ###   ########.fr       */
+/*   Updated: 2024/05/13 12:05:15 by bpisak-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,10 @@
 
 void	on_cd(t_exec e)
 {
-	(void)e;
-	// char *cwd;
+	char	*cwd;
 
-	// printf("cd");
-	// cwd = getcwd(NULL, 0);
-	// printf("")
-	// if (e.argc == 1)
-	// 	exit (e.exit_code);
-	// if (e.argc > 2)
-	// {
-	// 	printf("exit: too many arguments\n");
-	// 	exit (1);
-	// }
-	// if (!is_valid_int(e.argv[1]))
-	// {
-	// 	printf("exit: %s: numeric argument required\n", e.argv[0]);
-	// 	exit (2);
-	// }
-	// printf("exit\n");
-	// exit (ft_atoi(e.argv[1]));
+	if (e.argc == 1)
+		return ;
+	cwd = getcwd(NULL, 0);
+	chdir(e.argv[1]);
 }
